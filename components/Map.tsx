e05bc348706e010c3a83ab954f7296f6c919554e
Vedran Erak <vedran.erak@snapguest.si>
2024-08-24 18:31:05 +0200

    Home screen UI with mock data

---
+++ components/Map.tsx
@@ -0,0 +1,14 @@
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+
+export default function Map() {
+    return (
+        <MapView
+            provider={PROVIDER_DEFAULT}
+            className="w-full h-full rounded-2xl"
+            tintColor="black"
+            mapType="standard"
+            showsPointsOfInterest={false}
+            // initialRegion={region}
+            showsUserLocation={true}></MapView>
+    );
+}
